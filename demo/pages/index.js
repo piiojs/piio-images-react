8f95c17fff4c3debbf4339ef8babfbbffa33f3e8
Stella <stella.sblanco@gmail.com>
2020-06-16 16:32:01 -0300

    new demo
    for test and new component

--- demo/pages/index.js
+++ demo/pages/index.js
@@ -10,7 +10,15 @@ export default ()=>(
       <link rel="preconnect" href="//www.googletagmanager.com" />
       <link rel="preload" as="script" href="//js.piio.co/piio/piio.min.js" />
     </Head>
-    <script src="//js.piio.co/piio/piio.min.js"></script>
+    <script language="javascript">
+      (function(i,m,a,g,e) {
+          e = i.getElementsByTagName(m)[0], (g = i.createElement(m)).src = "//pcdn.piiojs.com/"+a+"/image.min.js",
+          g.onerror = function() {
+              (g = i.createElement(m)).src = "https://fs.piio.co/image-failover.min.js",
+              e.parentNode.insertBefore(g, e);
+          }, e.parentNode.insertBefore(g, e);
+      }(document, "script", "demo"));
+    </script>
     <PiioElement tag="img" path="https://www.publitas.com/blog/holiday-inspiration-great-examples-of-guides-and-christmas-catalogs/header.jpg" alt="hola" class="clase"></PiioElement>
     <PiioElement tag="a" path="https://www.publitas.com/blog/holiday-inspiration-great-examples-of-guides-and-christmas-catalogs/header.jpg">
       hola
